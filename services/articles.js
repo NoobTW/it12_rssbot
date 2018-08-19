6539be6755d8668ae794485599ddfc0274ae5c66
Coin <coin3x@users.noreply.github.com>
2018-08-19 13:19:21 +0800

    refactor: remove unnecessary wrapper

--- services/articles.js
+++ services/articles.js
@@ -4,54 +4,35 @@ const moment = require('moment');
 const parser = new Parser();
 moment.locale('zh-tw');
 
-const getRSS = (linkRss) => new Promise(async (resolve, reject) => {
-	try{
-		const feed = await parser.parseURL(linkRss);
-		resolve(feed);
-	}catch(e){
-		reject(e);
-	}
-});
+const getRSS = linkRss => parser.parseURL(linkRss);
 
-const getLatest = (linkRss) => new Promise(async (resolve, reject) => {
-	try{
-		const feed = await getRSS(linkRss);
-		const article = feed.items[0];
-		resolve({
-			siteTitle: feed.title,
-			...article,
-		});
-	}catch(e){
-		reject(e);
-	}
-});
+const getLatest = async (linkRss) => {
+	const feed = await getRSS(linkRss);
+	const article = feed.items[0];
+	return {
+		siteTitle: feed.title,
+		...article,
+	};
+};
 
-const getRecent = (linkRss, n = 7, unit = 'days') => new Promise(async (resolve, reject) => {
-	try{
-		const feed = await getRSS(linkRss);
-		const articles = feed.items.filter(x => Math.abs(moment(new Date()).diff(x.isoDate, unit)) <= n);
-		resolve({
-			siteTitle: feed.title,
-			articles,
-		});
-	}catch(e){
-		reject(e);
-	}
-});
+const getRecent = async (linkRss, n = 7, unit = 'days') => {
+	const feed = await getRSS(linkRss);
+	const articles = feed.items.filter(x => Math.abs(moment(new Date()).diff(x.isoDate, unit)) <= n);
+	return {
+		siteTitle: feed.title,
+		articles,
+	};
+};
 
-const getThisWeek = (linkRss) => new Promise(async (resolve, reject) => {
-	try{
-		const feed = await getRSS(linkRss);
-		const Sunday = moment(new Date()).day('Sunday').startOf('day');
-		const articles = feed.items.filter(x => Sunday.unix() < moment(x.isoDate).unix());
-		resolve({
-			siteTitle: feed.title,
-			articles,
-		});
-	}catch(e){
-		reject(e);
-	}
-});
+const getThisWeek = async (linkRss) => {
+	const feed = await getRSS(linkRss);
+	const Sunday = moment(new Date()).day('Sunday').startOf('day');
+	const articles = feed.items.filter(x => Sunday.unix() < moment(x.isoDate).unix());
+	return {
+		siteTitle: feed.title,
+		articles,
+	};
+};
 
 module.exports = {
 	getRSS,
