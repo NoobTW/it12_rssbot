8ba5ad48c11ec5ba890449ce5ef70b79b67eb54c
NoobTW <noob@noob.tw>
2018-08-19 14:44:04 +0800

    feat: all posts

--- server.js
+++ server.js
@@ -79,9 +79,9 @@ new CronJob('0 50 23 * * 6', async () => {
 
 bot.onText(/\/recents/, async (msg) => {
 	bot.sendChatAction(msg.chat.id, 'typing');
-	let message = '大家最近的新文章：\n';
+	let message = '最近的三篇新文章：\n';
 	let articles = await Promise.all(blogs.map(x => Articles.getLatest(x.link)));
-	articles = articles.sort((a, b) => moment(b.isoDate).unix() - moment(a.isoDate).unix());
+	articles = articles.sort((a, b) => moment(b.isoDate).unix() - moment(a.isoDate).unix()).slice(0, 3);
 	Array.from(articles).forEach(article => {
 		if(article){
 			const content = article.contentSnippet.length > 100 ?
@@ -91,7 +91,21 @@ bot.onText(/\/recents/, async (msg) => {
 		}
 	});
 	bot.sendMessage(msg.chat.id, message.trim(), {parse_mode: 'Markdown', disable_web_page_preview: true});
-})
+});
+
+bot.onText(/\/all/, async (msg) => {
+	bot.sendChatAction(msg.chat.id, 'typing');
+	let message = '大家最近的新文章：\n';
+	let articles = await Promise.all(blogs.map(x => Articles.getLatest(x.link)));
+	articles = articles.sort((a, b) => moment(b.isoDate).unix() - moment(a.isoDate).unix());
+	Array.from(articles).forEach(article => {
+		if(article){
+			message += `*${article.title}* ${moment(article.isoDate).fromNow()}\n`;
+			message += `🔗 ${article.link}\n\n`;
+		}
+	});
+	bot.sendMessage(msg.chat.id, message.trim(), {parse_mode: 'Markdown', disable_web_page_preview: true});
+});
 
 bot.onText(/\/about/, (msg) => {
 	bot.sendMessage(msg.chat.id, '鼓勵大家寫文章，所以我會定期檢查大家有沒有寫文章。\n歡迎送 PR；https://github.com/KP3am/rssbot');
